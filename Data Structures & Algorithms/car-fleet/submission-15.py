c0facddf7af33a0072c61fe4e94bdc1bed67b424
Vidhyabhushan Balaji <99535514+vidhyabhushanbalaji@users.noreply.github.com>
2026-08-20 01:46:18 +0100

    Add: car-fleet - submission-15

---
+++ Data Structures & Algorithms/car-fleet/submission-15.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [[p,s] for p,s in zip(position, speed)]
+        pair = sorted(pair)
+
+        top=[]
+        count=0
+
+        for i in range(len(pair)-1, -1, -1):
+            time = (target-pair[i][0])/pair[i][1]
+            if not top or time>top[-1]:
+                count+=1
+                top.append(time)
+        return (count)
